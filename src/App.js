973749c0d0dd806068d8c23f5596672f3705db58
Pierce <pierce@Pierces-MacBook-Pro-3.local>
2020-10-06 14:05:18 +0100

    Added alert if no search text added and search result stats

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +10,10 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
+    query: '',
   };
+
   // Lifecycle method - when app loads
   // async componentDidMount() {
   //   this.setState({ loading: true });
@@ -20,31 +24,38 @@ class App extends Component {
   // }
 
   // Search GitHub users
+
   searchUsers = async (text) => {
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data.items, loading: false });
+    this.setState({ users: res.data.items, loading: false, query: text });
   };
 
   clearUsers = () => {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   // render is a lifecycle method (runs a certain point when the app is loaded)
   render() {
-    const { loading, users } = this.state;
+    const { loading, users, query } = this.state;
     return (
       <div className='App'>
         <Navbar />
-
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
-          <Users loading={loading} users={users} />
+          <Users loading={loading} users={users} query={query} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,15 +3,22 @@ import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 
-const Users = ({ users, loading }) => {
+const Users = ({ users, loading, query }) => {
   if (loading) {
     return <Spinner />;
   } else {
     return (
-      <div style={userStyle}>
-        {users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
+      <div>
+        {users.length > 0 && (
+          <div className='results-stats'>
+            {users.length} results for {query}
+          </div>
+        )}
+        <div style={userStyle}>
+          {users.map((user) => (
+            <UserItem key={user.id} user={user} />
+          ))}
+        </div>
       </div>
     );
   }
@@ -24,9 +31,10 @@ Users.protoTypes = {
 
 const userStyle = {
   display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridTemplateColumns: 'repeat(4, 1fr)',
   gridGap: '1rem',
-  marginTop: '1rem',
+  marginTop: '0rem',
+  marginBottom: '0',
 };
 
 export default Users;
